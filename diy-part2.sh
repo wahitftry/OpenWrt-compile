63965d7a027e8ed44c14c961092ff194879a1632
Wahit Fitriyanto <26300406+wahitfitriyanto@users.noreply.github.com>
2022-08-09 19:59:33 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,3 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i "s/zh_cn/en/g" package/lean/default-settings/files/zzz-default-settings
-sed -i "s/+@LUCI_LANG_zh-cn/+@LUCI_LANG_en/g" package/lean/default-settings/Makefile
-sed -i "/po2lmo ./po/zh-cn/default.po/d" package/lean/default-settings/Makefile
