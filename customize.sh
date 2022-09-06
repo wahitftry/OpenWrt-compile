e8589fccd202c4346625bbe3c9f4405b018c220b
Wahit Fitriyanto <26300406+wahitfitriyanto@users.noreply.github.com>
2022-09-06 10:13:09 +0700

    Update the mechanism build

---
+++ customize.sh
@@ -0,0 +1,2 @@
+sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
